7a7e99bee108679a2a7c9b19e8ae534c25a74da0
Stuart Bowman <stuartsoft2012@gmail.com>
2015-11-16 19:46:57 -0500

    Board is built

--- Assets/Scripts/BoardPrefab.cs
+++ Assets/Scripts/BoardPrefab.cs
@@ -8,6 +8,7 @@ public class BoardPrefab : MonoBehaviour {
     public Player Player2;
     public GameObject BoardTilePrefab;
     List<List<GameObject>> BoardTiles;
+    GameObject BoardBackground;
 
     public float tileSpacing = 0.1f;
 
@@ -23,21 +24,35 @@ public class BoardPrefab : MonoBehaviour {
         for (int i = 0; i < mainBoard.boardSize-1; i++)
         {
             List<GameObject> row = new List<GameObject>();
+            float yprogress = (float)i / (mainBoard.boardSize - 1);
+
             for (int j = 0; j < mainBoard.boardSize-1; j++)
             {
                 GameObject temp = Instantiate(BoardTilePrefab, Vector3.zero, Quaternion.identity) as GameObject;
                 temp.transform.SetParent(transform);
                 float wid = -((mainBoard.boardSize - 1))*tileSize;
-                float progress = (float)j / (mainBoard.boardSize - 1);
-                float pos = ((progress) * wid) - wid/2.0f;
-                pos -= tileSize / 2;
-                pos *= 1.1f;
-                
-                temp.transform.position = new Vector3(pos, 0, 0);
+                float xprogress = (float)j / (mainBoard.boardSize - 1);
+                float xpos = ((xprogress) * wid) - wid/2.0f;
+                xpos -= tileSize / 2;
+                xpos *= 1.1f;
+
+                float zpos = ((yprogress) * wid) - wid / 2.0f;
+                zpos -= tileSize / 2;
+                zpos *= 1.1f;
+
+                temp.transform.position = new Vector3(xpos, 0, zpos);
                 row.Add(temp);
             }
             BoardTiles.Add(row);
         }
+
+        BoardBackground = Instantiate(BoardTilePrefab, Vector3.zero, Quaternion.identity) as GameObject;
+        float backgroundSize = (tileSize / 10.0f) * mainBoard.boardSize;
+        BoardBackground.transform.position = new Vector3(BoardBackground.transform.position.x, -0.01f, BoardBackground.transform.position.z);
+        BoardBackground.transform.localScale = new Vector3(backgroundSize, backgroundSize, backgroundSize);
+        Renderer rend = BoardBackground.GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.material.color = Constants.BLACKCOLOR;
     }
 	
 	// Update is called once per frame
